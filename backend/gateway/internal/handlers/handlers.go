942f24324b472a2fb2520c40dc96885b909d0d44
Andrei <actionclaw@gmail.com>
2024-07-13 19:08:43 +0200

    feat: add handler initialization

--- backend/gateway/internal/handlers/handlers.go
+++ backend/gateway/internal/handlers/handlers.go
@@ -1,9 +1,23 @@
 package handlers
 
-type Config struct{}
+import (
+	"go.uber.org/zap"
+	pb "workmap/gateway/internal/gapi/proto_gen"
+)
 
-type Handler struct{}
+type Config struct {
+	Logger *zap.Logger
+	Auth   pb.AuthServiceClient
+}
+
+type Handler struct {
+	logger *zap.Logger
+	auth   pb.AuthServiceClient
+}
 
 func New(cfg *Config) *Handler {
-	return &Handler{}
+	return &Handler{
+		logger: cfg.Logger,
+		auth:   cfg.Auth,
+	}
 }
