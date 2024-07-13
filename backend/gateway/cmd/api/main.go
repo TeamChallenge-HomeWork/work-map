9f3857a42b068441e5216b2729c4e5ddb321fb2a
Andrei <actionclaw@gmail.com>
2024-07-13 17:13:49 +0200

    feat: implement server running and graceful shutdown

--- backend/gateway/cmd/api/main.go
+++ backend/gateway/cmd/api/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"workmap/gateway/config"
 	"workmap/gateway/logger"
 )
@@ -8,5 +13,18 @@ import (
 func main() {
 	log := logger.New()
 	cfg := config.New(log)
-	_ = cfg
+	services := cfg.InitServices(log)
+	server := services.Server
+
+	server.Run()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	server.ShutDown(ctx)
 }
