11ccfa2871d9bdd1a72a1cdc126655507f2542db
Andrei <actionclaw@gmail.com>
2024-07-18 11:28:22 +0200

    feat: add function for extract exp from access token

--- backend/gateway/internal/handlers/user.go
+++ backend/gateway/internal/handlers/user.go
@@ -2,10 +2,14 @@ package handlers
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/status"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 	pb "workmap/gateway/internal/gapi/proto_gen"
 )
@@ -56,10 +60,13 @@ func (h *Handler) UserRegister(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO add JWT validator for access and refresh token
+	ttl, err := getTTL(res.AccessToken)
+	if err != nil {
+		h.logger.Error("failed to get ttl", zap.Error(err))
+		// TODO what am i suppose to do?
+	}
 
-	// TODO add TTL extractor from JWT
-	rRes := h.redis.Client.Set("access_token:"+res.AccessToken, u.Email, time.Duration(5)*time.Minute)
+	rRes := h.redis.Client.Set("access_token:"+res.AccessToken, u.Email, ttl)
 	if rRes.Err() != nil {
 		h.logger.Error("failed to set access token", zap.String("token", res.AccessToken))
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
@@ -87,4 +94,42 @@ func (h *Handler) UserRegister(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func getTTL(token string) (ttl time.Duration, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			ttl = 0
+			err = errors.New("failed to get ttl")
+		}
+	}()
+
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		return 0, errors.New("cannot split the token string")
+	}
+
+	payload, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		return 0, err
+	}
+
+	var payloadData map[string]interface{}
+	if err = json.Unmarshal(payload, &payloadData); err != nil {
+		return 0, err
+	}
+
+	exp, ok := payloadData["exp"]
+	if !ok {
+		return 0, errors.New("exp not found in the token")
+	}
+
+	expString := strconv.FormatFloat(exp.(float64), 'f', -1, 64)
+	i, err := strconv.ParseInt(expString, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	tExp := time.Unix(i, 0)
+
+	return tExp.Sub(time.Now()), nil
 }
